7fe5f1649eccc636821e4a1c649d052d5eb360ee
Lqnk4269 <95261280+Lqnk4269@users.noreply.github.com>
2022-11-02 09:19:34 -0700

    did the t;hing

--- BooleanFun.java
+++ BooleanFun.java
@@ -1,6 +1,12 @@
+import java.util.*;
 public class BooleanFun {
     public static void main(String[] args) {
-        int myAge = 35;
+
+        Scanner in = new Scanner(System.in);
+
+        int myAge = in.nextInt();
+
+        in.close();
 
         System.out.println("Current age: " + myAge);
 
@@ -9,7 +15,19 @@ public class BooleanFun {
         System.out.println("Gets a senior discount: "+ (myAge >= 65));
         System.out.println("Can order from children's menu: " + (myAge <=12));
 
-        System.out.println("On your next birthday out you will be: "); //complete this code, don't do math in sout
+        if(myAge >= 25) {
+            System.out.println("You can rent a car");
+        } else {
+            System.out.println("You can't rent a car");
+        }
+        if(myAge > 65) {
+            System.out.println("Get a senior discount");
+        }
+        if(myAge<=12) {
+            System.out.println("Can order from a children's menu");
+        }
+        myAge++;
+        System.out.println("On your next birthday out you will be: " + myAge); //complete this code, don't do math in out
 
         //further modification: prompt user for input and then print what they are able to do based on their age
         //and print their age on their next birthday
